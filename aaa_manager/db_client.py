f0f409da8f607070dd600e0d10ba7c52a1d25446
Paulo Silva <paulo308@users.noreply.github.com>
2017-07-16 14:16:13 +0100

    Update db_client.py

--- aaa_manager/db_client.py
+++ aaa_manager/db_client.py
@@ -15,9 +15,9 @@ from aaa_manager.exceptions import DBError
 LOG = logging.getLogger(__name__)
 
 #_DEFAULT_DB_HOST = "AUTH_DB_HOST" in os.environ
-_DEFAULT_DB_HOST = os.environ.get("AUTH_DB_HOST", "mongo")
+_DEFAULT_DB_HOST = os.getenv("AUTH_DB_HOST", "mongo")
 #_DEFAULT_DB_PORT = "AUTH_DB_PORT" in os.environ
-_DEFAULT_DB_PORT = int(os.environ.get("AUTH_DB_PORT", 27017))
+_DEFAULT_DB_PORT = int(os.getenv("AUTH_DB_PORT", 27017))
 
 #_DEFAULT_DB_HOST = 'mongo'
 #_DEFAULT_DB_PORT = 27017
