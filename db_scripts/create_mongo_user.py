0798973270f0cdf11a7fe7b3b2f3355b118646ad
Paulo Silva <paulo308@users.noreply.github.com>
2017-07-16 13:32:52 +0100

    Update create_mongo_user.py

--- db_scripts/create_mongo_user.py
+++ db_scripts/create_mongo_user.py
@@ -58,8 +58,8 @@ _CODE_CREATE_USER = """
     """
 
 
-_DEFAULT_DB_HOST = 'mongo'
-_DEFAULT_DB_PORT = 27017
+#_DEFAULT_DB_HOST = 'mongo'   Commented for testing env variables
+#_DEFAULT_DB_PORT = 27017
 _DEFAULT_DB_NAME = 'AAADB'
 _DEFAULT_CLIENT_CERT = 'certs/mongo_client_crt.pem'
 _DEFAULT_CA_CERT = 'certs/root_ca.pem'
