d2212d7d3905ed83640cc12323e9c9119e28fb50
Paulo Silva <paulo308@users.noreply.github.com>
2017-11-20 17:18:39 +0000

    Update create_mongo_user.py

--- db_scripts/create_mongo_user.py
+++ db_scripts/create_mongo_user.py
@@ -21,7 +21,7 @@ PWD="H1DiZeMWdU4UmA=="
 _DEFAULT_DB_PORT = 27017"""
 
 _DEFAULT_DB_HOST = os.getenv('AUTH_DB_HOST')
-_DEFAULT_DB_PORT = os.getenv('AUTH_DB_PORT')
+_DEFAULT_DB_PORT = int(os.getenv('AUTH_DB_PORT'))
 
 """_DEFAULT_DB_HOST = dbhost
 _DEFAULT_DB_PORT = dbport"""
