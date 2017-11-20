bcdca229dd9d642e3c1ccca22fff3cac4a4a17c6
Paulo Silva <paulo308@users.noreply.github.com>
2017-11-20 17:06:21 +0000

    Update create_mongo_user.py

--- db_scripts/create_mongo_user.py
+++ db_scripts/create_mongo_user.py
@@ -20,11 +20,11 @@ PWD="H1DiZeMWdU4UmA=="
 """_DEFAULT_DB_HOST = 'mongo'
 _DEFAULT_DB_PORT = 27017"""
 
-"""_DEFAULT_DB_HOST = os.getenv('AUTH_DB_HOST', mongo)
-_DEFAULT_DB_PORT = os.getenv('AUTH_DB_PORT', 27017)"""
+_DEFAULT_DB_HOST = os.getenv('AUTH_DB_HOST')
+_DEFAULT_DB_PORT = os.getenv('AUTH_DB_PORT')
 
-_DEFAULT_DB_HOST = dbhost
-_DEFAULT_DB_PORT = dbport
+"""_DEFAULT_DB_HOST = dbhost
+_DEFAULT_DB_PORT = dbport"""
 
 _DEFAULT_DB_NAME = 'AAADB'
 _DEFAULT_CLIENT_CERT = 'certs/mongo_client_crt.pem'
