052cbdf4d441d5ae63a6e00ea6e47851aa83d40b
Paulo Silva <paulo308@users.noreply.github.com>
2017-11-20 17:05:24 +0000

    Update db_client.py

--- aaa_manager/db_client.py
+++ aaa_manager/db_client.py
@@ -5,6 +5,7 @@ for establishing connections with MongoDB.
 """
 import logging
 import ssl
+import os
 
 from pymongo import MongoClient
 from pymongo.errors import ConnectionFailure
@@ -17,11 +18,11 @@ LOG = logging.getLogger(__name__)
 """_DEFAULT_DB_HOST = 'mongo'
 _DEFAULT_DB_PORT = 27017"""
 
-"""_DEFAULT_DB_HOST = os.getenv('AUTH_DB_HOST', mongo)
-_DEFAULT_DB_PORT = os.getenv('AUTH_DB_PORT', 27017)"""
+_DEFAULT_DB_HOST = os.getenv('AUTH_DB_HOST')
+_DEFAULT_DB_PORT = os.getenv('AUTH_DB_PORT')
 
-_DEFAULT_DB_HOST = dbhost
-_DEFAULT_DB_PORT = dbport
+"""_DEFAULT_DB_HOST = dbhost
+_DEFAULT_DB_PORT = dbport"""
 
 _DEFAULT_DB_NAME = 'AAADB'
 _DEFAULT_CLIENT_CERT = 'certs/mongo_client_crt.pem'
