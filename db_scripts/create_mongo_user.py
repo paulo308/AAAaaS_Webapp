18065d9a93fc51c89aab9e20647ad405e577836d
Paulo Silva <paulo308@users.noreply.github.com>
2017-11-20 15:33:22 +0000

    env_vars

--- db_scripts/create_mongo_user.py
+++ db_scripts/create_mongo_user.py
@@ -19,8 +19,13 @@ db.getSiblingDB('$external').runCommand(
 PWD="H1DiZeMWdU4UmA=="
 """_DEFAULT_DB_HOST = 'mongo'
 _DEFAULT_DB_PORT = 27017"""
-_DEFAULT_DB_HOST = os.getenv('AUTH_DB_HOST', mongo)
-_DEFAULT_DB_PORT = os.getenv('AUTH_DB_PORT', 27017)
+
+"""_DEFAULT_DB_HOST = os.getenv('AUTH_DB_HOST', mongo)
+_DEFAULT_DB_PORT = os.getenv('AUTH_DB_PORT', 27017)"""
+
+_DEFAULT_DB_HOST = dbhost
+_DEFAULT_DB_PORT = dbport
+
 _DEFAULT_DB_NAME = 'AAADB'
 _DEFAULT_CLIENT_CERT = 'certs/mongo_client_crt.pem'
 _DEFAULT_CA_CERT = 'certs/root_ca.pem'
